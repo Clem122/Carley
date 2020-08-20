ddba207570cf1dd90b8908ee3f970604fb9a1f15
Clem122 <69987127+Clem122@users.noreply.github.com>
2020-08-20 22:25:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,24 +2,24 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on("message", (message) => {
-  // Exit and stop if it's not there
-    if (!message.content.startsWith(prefix)) return;
-
-  if (message.content.startsWith(prefix + "lenny")) {
-    message.channel.sendMessage("( ͡° ͜ʖ ͡°)");
-    message.react(":lenny:304276195594207232");
-  } else
-  if (message.content.startsWith(prefix + "franio")) {
-    message.channel.sendFile('https://i.imgur.com/FHxjllM.png', 'TwojaStara.png', 'testmessage');
-    message.react("franciszek:383357843799277568")
-  } else
-  if (message.content.startsWith(prefix + "help")) {
-    message.react("oko:325409745278337024");
-    message.channel.sendMessage(":mailbox: Sprawdz swoją skrzynke odbiorczą");
-    message.author.sendMessage("chuj ci w dupe XD");
-  } 
-   
+client.on('message', message => {
+  if (message.content === prefix + 'ping') {
+    message.channel.send({embed: {
+  color: 101981,
+  description: (':ping_pong:  `' + `${Date.now() - message.createdTimestamp}` + ' ms`'),
+  title: " ",
+  author: {
+  name: "Twój ping to", 
+  icon_url: message.author.avatarURL
+    },
+  image: {
+  "url": " ",
+    },
+  thumbnail: {
+  "url": " "
+    },
+}});
+  }
 });
 
 // NOTE: CURRENTLY BROKEN IN DISCORD.JS 9 THROUGH 11.1. USE THIS INSTEAD:
